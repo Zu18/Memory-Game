64584663d20dc3ff0ea62e3e6c63f65fd2ded9b4
Zukhra <zuhra.galieva.2014@gmail.com>
2023-06-09 13:35:55 +0100

    Open two cards, compare them,keep open matching cards or close the unmatching cards after a short delay

--- my-script.js
+++ my-script.js
@@ -13,6 +13,9 @@ const cardValues = [
   'images/3_of_spades.png', 'images/3_of_spades.png'
 ];
 
+// Create an array to keep the open cards
+let openCards = [];
+
 
 // Get the memory game container element
 const memoryGameContainer = document.querySelector('.memory-game');
@@ -28,9 +31,36 @@ for (let i = 0; i < numCards; i++) {
   card.appendChild(image);
   memoryGameContainer.appendChild(card);
 
-  // Show/hide  each card's value (image) on click
-  card.addEventListener('click', function() {
-    this.classList.toggle('show');
+  // Show/hide/match each card's value (image) on click
+  card.addEventListener('click', function () {
+    console.log('Card clicked')
+    if (openCards.length < 2 && !this.classList.contains('show') && !this.classList.contains('matched')) {
+      console.log('This card is not shown')
+      this.classList.add('show');
+      openCards.push(this);
+      console.log('Show the card')
+      if (openCards.length === 2) {
+        console.log('Two cards are open')
+        // Get the card values for comparison
+        const cardValue1 = openCards[0].querySelector('img').src;
+        const cardValue2 = openCards[1].querySelector('img').src;
+        console.log(`cardValue1 ${cardValue1} and cardValue2 ${cardValue2}`)
+        if (cardValue1 === cardValue2) {
+          console.log('Cards are the same')
+          // If the cards match, mark them as matched
+          openCards.forEach(card => card.classList.add('matched'));
+          openCards = [];
+          console.log('Cards matched')
+        } else {
+          console.log('Cards are different')
+          // If the cards don't match, hide them after a short delay
+          setTimeout(() => {
+            openCards.forEach(card => card.classList.remove('show'));
+            openCards = [];
+            console.log('Cards closed')
+          }, 1000);
+        }
+      }
+    }
   });
 }
-
